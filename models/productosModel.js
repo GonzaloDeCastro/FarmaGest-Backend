27c49fe95f9809f9b9c046bebdaf09d856d23476
Gonzalo De Castro <gonzalodecastro1@gmail.com>
2025-02-15 17:16:42 -0300

    Se agrega mensaje en detalle para el agregado y eliminado de productos para la tabla de auditoria_productos

--- models/productosModel.js
+++ models/productosModel.js
@@ -45,7 +45,7 @@ class Producto {
           return callback(err, null);
         }
         db.query(
-          "INSERT INTO auditoria_productos (producto_id, accion, detalle_cambio, fecha_movimiento, usuario_id) VALUES (?, 'CREAR', '-', NOW(), ?)",
+          "INSERT INTO auditoria_productos (producto_id, accion, detalle_cambio, fecha_movimiento, usuario_id) VALUES (?, 'CREAR', 'Se ha creado un nuevo producto', NOW(), ?)",
           [result.insertId, usuario_id], // usuario_id por defecto 1 si no está disponible
           (err) => {
             if (err) {
@@ -127,7 +127,7 @@ class Producto {
 
   static eliminarProducto(producto_id, usuario_id, callback) {
     db.query(
-      "INSERT INTO auditoria_productos (producto_id, accion, fecha_movimiento,detalle_cambio, usuario_id) VALUES (?, 'ELIMINAR', NOW(),'-',?)",
+      "INSERT INTO auditoria_productos (producto_id, accion, fecha_movimiento,detalle_cambio, usuario_id) VALUES (?, 'ELIMINAR', NOW(),'Se ha eliminado este producto',?)",
       [producto_id, usuario_id],
       (err) => {
         if (err) {
