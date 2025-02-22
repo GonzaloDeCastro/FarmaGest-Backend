48fd91a72291bd3b6adfa7517bcc1851fa837cab
Gonzalo De Castro <gonzalodecastro1@gmail.com>
2025-02-22 18:47:33 -0300

    Se cambia configuracion de vercel json para solucionar problemas de cors

--- index.js
+++ index.js
@@ -7,21 +7,7 @@ const port = 3001;
 const routes = require("./routes/routes.js"); // Cambiado a plural para reflejar múltiples rutas
 
 app.use(express.json());
-const corsOptions = {
-  origin: function (origin, callback) {
-    const allowedOrigins = [
-      "https://farma-gest.vercel.app",
-      "https://anotherdomain.com",
-    ];
-    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-};
-
-app.use(cors(corsOptions));
+app.use(cors());
 
 // Rutas
 app.use("/api", routes); // Monta las rutas definidas en routes.js en el endpoint '/'
