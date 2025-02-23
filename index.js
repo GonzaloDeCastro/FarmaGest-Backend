ae9934600bb86e90d1a8aadee5149be8541ed2c3
Gonzalo De Castro <gonzalodecastro1@gmail.com>
2025-02-23 20:19:45 -0300

    se agrega puerto a archivo .env

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // index.js
-
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const port = 3001;
+const port = process.env.port || 3000;
 const routes = require("./routes/routes.js"); // Cambiado a plural para reflejar múltiples rutas
 app.use(express.json());
 app.use(cors());
